87a02e7f5561738d89f53b3c4deb23603ac029e0
Anderson0202 <53060401+Anderson0202@users.noreply.github.com>
2020-09-08 02:57:07 -0500

    Le aplique una nueva clase llamada pulsacion para resolver el ejecicio de forma orientada a objetos

--- Pulsaciones/Pulsacion.cs
+++ Pulsaciones/Pulsacion.cs
@@ -11,27 +11,34 @@ namespace Pulsaciones
 
         public void CalculaPulso()
         {
+
             Persona p = new Persona();
-            Console.WriteLine("             Bienvenid@");
-            Console.WriteLine("     _Calcule Sus Pulsaciones_");
-            Console.WriteLine(" Se necesita que ingrese dos Datos: ");
-            Console.WriteLine("#1 Edad: ");
+            Console.WriteLine("\t\t            Bienvenid@\n\n");
+            Console.WriteLine("\t\t     _Calcule Sus Pulsaciones_\n");
+            Console.WriteLine("\t\t Se necesita que ingrese dos Datos: ");
+            Console.WriteLine("\n #1 Edad: ");
             p.edad = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("#2 Sexo: ");
+            Console.WriteLine("\n\t\t Oprima la Tecla [_F_] Femenino ");
+            Console.WriteLine("\n\t\t Oprima la Tecla [_M_] Masculino");
+            Console.WriteLine("\n #2 Sexo: ");
             p.sexo = Convert.ToChar(Console.ReadLine());
 
-            if (p.sexo.Equals('f'))
-            {
-                Console.WriteLine("Su Pulsacion Cada 10 segundos es: " + (220 - p.edad) / 10);
-            }
-            else
-            {
-               Console.WriteLine ("Su Pulsacion cada 10 segundo es: " + (210 - p.edad) / 10);
-            }
+            mensaje(p.edad, p.sexo);
 
             Console.ReadLine();
 
+        }
 
+        private void mensaje(int edad, char sexo) {
+
+            if (sexo.Equals('f'))
+            {
+                Console.WriteLine("\nSu Pulsacion Cada 10 segundos son: " + (220 - edad) / 10);
+            }
+            else
+            {
+                Console.WriteLine("Su Pulsacion cada 10 segundos son: " + (210 - edad) / 10);
+            }
 
         }
 
